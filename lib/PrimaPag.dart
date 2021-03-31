ff81dc01e1a57e4fe8782002694c4be72303f662
s-stasi <samuele.stasi2002@gmail.com>
2021-03-31 18:23:54 +0200

    Adding spaces betweencomponents

--- lib/PrimaPag.dart
+++ lib/PrimaPag.dart
@@ -7,44 +7,76 @@ class PrimoProblema extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: SideDrawer(),
-      appBar: AppBar(
-        title: Text('PRIMO PROBLEMA'),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.black,
-                Colors.blue,
-              ]
-            )
-          ), 
-        ), 
-      ),
-      body: Row(
-        children: <Widget>[
+        drawer: SideDrawer(),
+        appBar: AppBar(
+          title: Text('PRIMO PROBLEMA'),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Colors.black,
+              Colors.blue,
+            ])),
+          ),
+        ),
+        body: Row(children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20
+          ),
           Expanded(
             child: TextField(
-              decoration: InputDecoration(enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue.shade900,width: 3.0)),labelText: 'TC',),
-             )
-           ),
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.blue.shade900, width: 3.0)),
+                labelText: 'TC',
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20
+          ),
           Expanded(
             child: TextField(
-              decoration: InputDecoration(enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue.shade900,width: 3.0)),labelText: 'TAS',),
-             )
-           ),
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.blue.shade900, width: 3.0)),
+                labelText: 'TAS',
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20
+          ),
           Expanded(
-            child: TextField(
-              decoration: InputDecoration(enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue.shade900,width: 3.0)),labelText: 'WIND VELOCITY',),
-             )
-           ),
+              child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.blue.shade900, width: 3.0)),
+              labelText: 'WIND VELOCITY',
+            ),
+          )),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20
+          ),
           Expanded(
-            child: TextField(
-              decoration: InputDecoration(enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue.shade900,width: 3.0)),labelText: 'WIND ANGLE',),
-             )
-           ),
-        ]
-      )
-    );
+              child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.blue.shade900, width: 3.0)),
+              labelText: 'WIND ANGLE',
+            ),
+          )),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20
+          ),
+        ]));
   }
 }
