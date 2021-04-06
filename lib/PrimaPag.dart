a535b942c5ab09a2a1e28db41d81cb9f40f6a76c
s-stasi <samuele.stasi2002@gmail.com>
2021-04-06 14:02:19 +0200

    Changing button position

--- lib/PrimaPag.dart
+++ lib/PrimaPag.dart
@@ -27,96 +27,102 @@ class _PPState extends State<PrimoProblema> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: SideDrawer(),
-        appBar: AppBar(
-          title: Text('PRIMO PROBLEMA'),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-              Colors.black,
-              Colors.blue,
-            ])),
-          ),
+      drawer: SideDrawer(),
+      appBar: AppBar(
+        title: Text('PRIMO PROBLEMA'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.black,
+            Colors.blue,
+          ])),
         ),
-        body: Row(children: <Widget>[
-          Container(
+      ),
+      body: Row(children: <Widget>[
+        Container(
             width: MediaQuery.of(context).size.width * 0.04,
-            height: MediaQuery.of(context).size.height * 0.20
-          ),
-          Expanded(
-            child: TextField(
-              controller: tc,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.blue.shade900, width: 3.0)),
-                labelText: 'TC',
-              ),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.04,
-            height: MediaQuery.of(context).size.height * 0.20
-          ),
-          Expanded(
-            child: TextField(
-              controller: tas,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.blue.shade900, width: 3.0)),
-                labelText: 'TAS',
-              ),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.04,
-            height: MediaQuery.of(context).size.height * 0.20
-          ),
-          Expanded(
-            child: TextField(
-              controller: windAngle,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                    BorderSide(color: Colors.blue.shade900, width: 3.0)),
-                labelText: 'WIND ANGLE',
-              ),
-            )
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.04,
-            height: MediaQuery.of(context).size.height * 0.20
-          ),
-          Expanded(
-              child: TextField(
-              controller: windVel,
+            height: MediaQuery.of(context).size.height * 0.20),
+        Expanded(
+          child: TextField(
+            controller: tc,
             decoration: InputDecoration(
               enabledBorder: OutlineInputBorder(
                   borderSide:
                       BorderSide(color: Colors.blue.shade900, width: 3.0)),
-              labelText: 'WIND VELOCITY',
+              labelText: 'TC',
             ),
-          )),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.04,
-            height: MediaQuery.of(context).size.height * 0.20
           ),
-          FloatingActionButton(
-            onPressed: () => showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Text(
-                    'gs: ' + primoProblema(int.parse(tc.text), int.parse(tas.text), int.parse(windAngle.text), int.parse(windVel.text))[0].toString() + ' th: ' + primoProblema(int.parse(tc.text), int.parse(tas.text), int.parse(windAngle.text), int.parse(windVel.text))[1].toString()
-                  )
-                );
-              }
+        ),
+        Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20),
+        Expanded(
+          child: TextField(
+            controller: tas,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.blue.shade900, width: 3.0)),
+              labelText: 'TAS',
             ),
-            child: Text('='),
-          )
-        ]
-      )
+          ),
+        ),
+        Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20),
+        Expanded(
+            child: TextField(
+          controller: windAngle,
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Colors.blue.shade900, width: 3.0)),
+            labelText: 'WIND ANGLE',
+          ),
+        )),
+        Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20),
+        Expanded(
+            child: TextField(
+          controller: windVel,
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Colors.blue.shade900, width: 3.0)),
+            labelText: 'WIND VELOCITY',
+          ),
+        )),
+        Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            height: MediaQuery.of(context).size.height * 0.20),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                  content: Text('gs: ' +
+                      primoProblema(
+                              int.parse(tc.text),
+                              int.parse(tas.text),
+                              int.parse(windAngle.text),
+                              int.parse(windVel.text))[0]
+                          .toString() +
+                      ' th: ' +
+                      primoProblema(
+                              int.parse(tc.text),
+                              int.parse(tas.text),
+                              int.parse(windAngle.text),
+                              int.parse(windVel.text))[1]
+                          .toString()));
+            }),
+        child: Text('   =   '),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
