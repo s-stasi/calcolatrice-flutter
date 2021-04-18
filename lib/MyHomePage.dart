2abaee4292511f0f4a30f0832178e769ab4074d0
fexpax344 <federicopace2004@gmail.com>
2021-04-18 20:43:12 +0200

    prova

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -6,7 +6,33 @@ import 'SideDrawer.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+   var userInput = '';
+   var answer = '';
+   final List<String> buttons = [
+    'C',
+    '+/-',
+    '%',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    'x',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];// Array of button
+     
+     
+     Widget build(BuildContext context) {
     return Scaffold(
       drawer: SideDrawer(),
       appBar: AppBar(
@@ -21,8 +47,7 @@ class MyHomePage extends StatelessWidget {
             )
           ),
         ),  
-      ),
-      
+      ),  
       body: 
        Column(children: [
          Expanded(child: Container(child: Column(
@@ -31,14 +56,14 @@ class MyHomePage extends StatelessWidget {
                Container( padding: EdgeInsets.all(20),
                       alignment: Alignment.centerRight,
                       child: Text(
-                        'userInput',
+                        userInput,
                         style: TextStyle(fontSize: 18, color: Colors.white),
                       ),
                       ),
                       Container(padding: EdgeInsets.all(15),
                       alignment: Alignment.centerRight,
                       child: Text(
-                        'answer',
+                        answer,
                         style: TextStyle(
                             fontSize: 30,
                             color: Colors.white,
@@ -51,12 +76,136 @@ class MyHomePage extends StatelessWidget {
          ),
          Expanded(flex: 3,
          child: Container(
-        child: GridView.builder(itemCount: buttons.lenght, itemBuilder: itemBuilder)
-         )
+           child:GridView.builder(itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    // Clear Button
+                    if (index == 0) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput = '';
+                            answer = '0';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+  
+                    // +/- button
+                    else if (index == 1) {
+                      return MyButton(
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    // % Button
+                    else if (index == 2) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    // Delete Button
+                    else if (index == 3) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput =
+                                userInput.substring(0, userInput.length - 1);
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    // Equal_to Button
+                    else if (index == 18) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.orange[700],
+                        textColor: Colors.white,
+                      );
+                    }
+  
+                    //  other buttons
+                    else {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Colors.blueAccent
+                            : Colors.white,
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.black,
+                      );
+                    }
+                  })
+           
+         ),
+         
          )
        ],
        )
       
+    );
+  }
+}
+class MyButton extends StatelessWidget {
+    
+  // declaring variables 
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttontapped;
+  
+  //Constructor
+  MyButton({this.color, this.textColor, this.buttonText, this.buttontapped});
+  
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttontapped,
+      child: Padding(
+        padding: const EdgeInsets.all(0.2),
+        child: ClipRRect(
+          // borderRadius: BorderRadius.circular(25),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
