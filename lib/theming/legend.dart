8d12bd2a263087bbb5a07f68dcc7516df0595bc4
Samuele Stasi <samuele.stasi2002@gmail.com>
2021-05-25 21:14:15 +0200

    Fuck flutter and containers

--- lib/theming/legend.dart
+++ lib/theming/legend.dart
@@ -5,65 +5,71 @@ class Legend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: MediaQuery.of(context).size.height * 0.079,
+      padding: EdgeInsets.all(20),
       margin: EdgeInsets.all(25.0),
-      child: Column(children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.brightness_1,
-              color: Colors.red,
-              size: 20,
-            ),
-            SizedBox(width: 10),
-            Text(
-              'ground speed',
-              style: TextStyle(
-                  color: Colors.red, fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.brightness_1,
-              color: Colors.black,
-              size: 20,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              'true air speed',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.brightness_1,
-              color: Colors.yellow,
-              size: 20,
-            ),
-            SizedBox(width: 10),
-            Text(
-              ' wind velocity',
-              style: TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ]),
+      height: 100,
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.brightness_1,
+                color: Colors.red,
+                size: 20,
+              ),
+              SizedBox(width: 10),
+              Text(
+                'ground speed',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.brightness_1,
+                color: Colors.black,
+                size: 20,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'true air speed',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.brightness_1,
+                color: Colors.yellow,
+                size: 20,
+              ),
+              SizedBox(width: 10),
+              Text(
+                'Wind velocity',
+                style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+      ),
       decoration: BoxDecoration(
           color: Colors.blueAccent[100],
           border: Border.all(
